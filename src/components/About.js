7bf3645e7757987a4966bdfb1deb380e169c813b
haroldmoj-dev <haroldmojica.dev@gmail.com>
2025-11-07 20:02:04 +0800

    Add content for About section

--- src/components/About.js
+++ src/components/About.js
@@ -7,15 +7,15 @@ export default function About() {
       <h2 className="text-4xl text-primary mb-6">About</h2>
       
       <p className="mb-4">
-        lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem 
+        I’m currently studying BS Computer Science, Major in Software Technology at De La Salle University (Manila). My journey into tech started in senior high while studying in Bahrain, when a capstone project about cheating prevention made me realize how much I enjoy solving problems through code.
       </p>
 
       <p className="mb-4">
-        lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem 
+        Since then, I’ve learned quite a lot of stuff from basic programming to exploring machine learning. I’m still discovering my niche, but I love the process of building things even on the side. In other words, curiosity drives me, and creativity keeps me going.
       </p>
 
       <p>
-        lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem 
+        But of course, I'm human! Outside of coding, I enjoy art, movies, music, and spending time with family. Hopefully, life in the future remains just as fulfilling.
       </p>
     </section>
   );
