59d9368c90d4a73967cd3094d804168ea1459d2b
haroldmoj-dev <haroldmojica.dev@gmail.com>
2025-11-09 02:49:54 +0800

    Refine skills box

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,41 +1,39 @@
 import { useState } from "react";
 
 export default function Skills() {
-  const [activeTab, setActiveTab] = useState("frontend");
+  const [activeTab, setActiveTab] = useState("interactive");
 
   return (
     <section
       id="skills"
-      className="scroll-mt-24 flex flex-col justify-center items-start px-4 md:px-6 lg:px-8 bg-background text-text max-w-3xl mx-auto py-16"
+      className="scroll-mt-24 flex flex-col justify-center items-start px-6 lg:px-8 bg-background text-text max-w-3xl mx-auto py-16"
     >
       <h2 className="text-4xl text-primary mb-6">Skills</h2>
 
-      <div className="w-full border border-gray-300 rounded-md overflow-hidden">
-        <div className="flex">
+      <div className="w-full overflow-hidden">
+        <div className="flex flex-wrap w-full">
           <button
-            className={`flex-1 py-2 text-center font-medium transition-colors ${
-              activeTab === "frontend"
-                ? "bg-secondary text-background text-bold"
-                : "bg-background text-text text-bold hover:bg-gray-100"
+            className={`py-2 grow sm:grow-0 text-center font-bold rounded-t-lg border-text border-t-2 border-l-2 border-r-2 transition-[padding] duration-200 ease-in-out ${
+              activeTab === "list" ? "px-4 text-text hover:text-secondary border-transparent" : "px-8 text-secondary bg-accent"
             }`}
-            onClick={() => setActiveTab("frontend")}
+            onClick={() => setActiveTab("interactive")}
           >
-            Frontend
+            Interactive Mode
           </button>
           <button
-            className={`flex-1 py-2 text-center font-medium transition-colors ${
-              activeTab === "backend"
-                ? "bg-secondary text-background text-bold"
-                : "bg-background text-text text-bold hover:bg-gray-100"
+            className={`py-2 grow sm:grow-0 text-center font-bold rounded-t-lg border-text border-t-2 border-l-2 border-r-2 transition-[padding] duration-200 ease-in-out ${
+              activeTab === "interactive" ? "px-4 text-text hover:text-secondary border-transparent" : "px-8 text-secondary bg-accent"
             }`}
-            onClick={() => setActiveTab("backend")}
+            onClick={() => setActiveTab("list")}
           >
-            Backend
+            List Mode
           </button>
         </div>
-
-        <div className="p-6 bg-background">
-          {activeTab === "frontend" && (
+        <div 
+          className={`p-6 bg-accent border-text border-2 rounded-b-lg ${
+            activeTab === "interactive" ? "sm:rounded-r-lg" : "sm:rounded-t-lg"
+          }`}>
+          {activeTab === "list" && (
             <div className="space-y-2">
               <p>React</p>
               <p>Tailwind CSS</p>
@@ -43,7 +41,7 @@ export default function Skills() {
               <p>Next.js</p>
             </div>
           )}
-          {activeTab === "backend" && (
+          {activeTab === "interactive" && (
             <div className="space-y-2">
               <p>Node.js</p>
               <p>Express</p>
