e178218d9627f217a013eb815d212ccdd52098bc
haroldmoj-dev <haroldmojica.dev@gmail.com>
2025-11-10 15:46:38 +0800

    Update project-t

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import TaeImg from "../assets/projects/tae.png";
+import ProjectTImg from "../assets/projects/project-t.png";
 import FpImg from "../assets/projects/fp.png";
 import FarmImg from "../assets/projects/farm.png";
 import SpringImg from "../assets/projects/spring.png";
@@ -11,56 +11,56 @@ import { FaGithub } from "react-icons/fa";
 const projects = [
   {
     title: "CityShadows",
-    description: "Routing tool recommending shaded walking paths",
+    description: "Routing tool recommending shaded walking paths.",
     link: "https://github.com/haroldmoj-dev/dlsu-cityshadows",
     image: CityImg,
     stack: ["Python", "PyTorch", "OpenCV", "GeoPandas"],
   },
   {
     title: "UniRide",
-    description: "Carpooling mobile application made for students",
+    description: "Carpooling mobile application made for students.",
     link: "https://github.com/haroldmoj-dev/dlsu-mobile-carpooling",
     image: UnirideImg,
     stack: ["Android Studio", "Java", "XML", "Firebase"],
   },
   {
     title: "JPDGarage",
-    description: "Online management system for motorcycle parts",
+    description: "Online management system for motorcycle parts.",
     link: "https://github.com/haroldmoj-dev/dlsu-jpdgarage",
     image: JpdImg,
     stack: ["React", "Node.js", "Express", "MongoDB"],
   },
   {
     title: "SpringTales",
-    description: "Website storing infographics made for Guardian Tales",
+    description: "Website storing infographics about the game 'Guardian Tales'.",
     link: "https://github.com/haroldmoj-dev/gt-central",
     image: SpringImg,
     stack: ["HTML/CSS"],
   },
   {
     title: "OpenChan",
-    description: "Web application demonstrating role-based access control system",
+    description: "Web application demonstrating role-based access control system.",
     link: "https://github.com/haroldmoj-dev/dlsu-secure-web-dev",
     image: OpenchanImg,
     stack: ["Node.js", "Express", "SQLite", "HTML/CSS", "Tailwind CSS"],
   },
   {
     title: "Project T",
-    description: "2D pixel game designed for 1–2 players (local multiplayer)",
-    link: "https://github.com/haroldmoj-dev/gamemaker-game1",
-    image: TaeImg,
+    description: "2D pixel game designed for 1–2 players (local multiplayer).",
+    link: "https://github.com/haroldmoj-dev/gamemaker-project-t",
+    image: ProjectTImg,
     stack: ["GameMaker Language"],
   },
   {
     title: "Floating Point Converter",
-    description: "Web application implementing Decimal32 floating point conversion",
+    description: "Web application implementing Decimal32 floating point conversion.",
     link: "https://github.com/haroldmoj-dev/dlsu-decimal32-fp-converter",
     image: FpImg,
     stack: ["Python", "Panel"],
   },
   {
     title: "MyFarm",
-    description: "Farming game showcasing object-oriented programming",
+    description: "Farming game showcasing object-oriented programming.",
     link: "https://github.com/haroldmoj-dev/dlsu-myfarm",
     image: FarmImg,
     stack: ["Java", "Swing"],
@@ -76,7 +76,10 @@ export default function Projects() {
       <h2 className="text-4xl text-primary mb-8">Projects</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 w-full">
         {projects.map((proj) => (
-          <div className="rounded-lg bg-accent transition relative overflow-hidden border-2 border-transparent hover:border-text hover:-translate-y-4 flex flex-col">
+          <div 
+            key={proj.title}
+            className="rounded-lg bg-accent transition relative overflow-hidden border-2 border-transparent hover:border-text hover:-translate-y-4 flex flex-col"
+          >
             <div
               className="min-h-60 flex flex-col relative"
               style={{
@@ -105,10 +108,7 @@ export default function Projects() {
                 ))}
               </div>
             </div>
-            <div 
-              key={proj.title}
-              className="relative z-10 bg-accent flex-grow"
-            >
+            <div className="relative z-10 bg-accent flex-grow">
               <div className="p-4">
                 <h3 className="text-lg font-semibold text-secondary mb-2">
                   {proj.title}
